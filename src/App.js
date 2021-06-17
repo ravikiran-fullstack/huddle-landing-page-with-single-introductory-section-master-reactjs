806995b70300ceaac672c93e13aa78ce25ebe1eb
ravikiran <ravikiransjce.jobs@gmail.com>
2021-06-17 12:35:40 +0530

    finished desktop verion, need to work on mobile responsiveness

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <div className="logo"></div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-7 illustration">
+          <div className=""></div>
+        </div>
+        <div className="col-md-5 pageInfo">
+          <div className="headingDiv">
+            <h1>Build The Community Your Fans Will Love</h1>
+          </div>
+          <div className="infoDiv">
+            <p>
+              Huddle re-imagines the way we build communities. You have a voice,
+              but so does your audience.Create connections with your users as
+              you engage in genuine discussion.
+            </p>
+          </div>
+          <div className="registerDiv">
+            <button>Register</button>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 socialMediaIcons">
+          <div className="facebookIcon">
+            <i className="fab fa-facebook-f"></i>
+          </div>
+          <div className="twitterIcon">
+            <i className="fab fa-twitter"></i>
+          </div>
+          <div className="instagramIcon">
+            <i className="fab fa-instagram"></i>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
